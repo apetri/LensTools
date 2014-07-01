8382cebe75c28f573097ccdeaa9335b3af6cc125
apetri <apetri@phys.columbia.edu>
2014-07-01 15:15:58 -0400

    Adding moments functionality to ConvergenceMap

--- lenstools/topology.py
+++ lenstools/topology.py
@@ -10,6 +10,8 @@
 
 """
 
+from __future__ import division
+
 from external import _topology
 
 import numpy as np
@@ -36,7 +38,7 @@ class ConvergenceMap(object):
 
 	def __init__(self,map_filename):
 
-		#Open the map file and store the dara in the class instance
+		#Open the map file and store the dara in the class instance; maybe change the implementation so it supports fitsio too?
 		self._map_filename = map_filename
 		fits_file = fits.open(map_filename)
 		self.kappa = fits_file[0].data.astype(np.float)
@@ -115,7 +117,7 @@ class ConvergenceMap(object):
 
 		:raises: AssertionError if thresholds array is not provided
 
-		>>> map = ConvergenceMap("map.fits")
+		>>> map = ConvergenceMap("map.fit")
 		>>> thresholds = np.arange(-2.0,2.0,0.2)
 		>>> V0,V1,V2 = map.minkowski(thresholds,norm=True)
 
@@ -137,3 +139,69 @@ class ConvergenceMap(object):
 		#Compute the Minkowski functionals and return them as tuple
 		return _topology.minkowski(self.kappa,self.gradient_x,self.gradient_y,self.hessian_xx,self.hessian_yy,self.hessian_xy,thresholds,sigma)
 
+	def moments(self,connected=False,dimensionless=False):
+
+		"""
+		Measures the first nine moments of the convergence map (two quadratic, three cubic and four quartic)
+
+		:param connected: if set to True returns only the connected part of the moments
+		:type connected: bool.
+
+		:param dimensionless: if set to True returns the dimensionless moments, normalized by the appropriate powers of the variance
+		:type dimensionless: bool. 
+
+		:returns: array -- (sigma0,sigma1,S0,S1,S2,K0,K1,K2,K3)
+
+		>>> map = ConvergenceMap("map.fit")
+		>>> var0,var1,sk0,sk1,sk2,kur0,kur1,kur2,kur3 = map.moments()
+		>>> sk0,sk1,sk2 = map.moments(dimensionless=True)[2:5]
+		>>> kur0,kur1,kur2,kur3 = map.moments(connected=True,dimensionless=True)[5:]
+
+		"""
+
+		#First check that the instance has the gradient and hessian attributes; if not, compute them
+		if not (hasattr(self,"gradient_x") and hasattr(self,"gradient_y")):
+			self.gradient()
+
+		if not (hasattr(self,"hessian_xx") and hasattr(self,"hessian_yy") and hasattr(self,"hessian_xy")):
+			self.hessian()
+		
+		#Quadratic moments
+		sigma0 = self.kappa.std()
+		sigma1 = np.sqrt((self.gradient_x**2 + self.gradient_y**2).mean())
+
+		#Cubic moments
+		S0 = (self.kappa**3).mean()
+		S1 = ((self.kappa**2)*(self.hessian_xx + self.hessian_yy)).mean()
+		S2 = ((self.gradient_x**2 + self.gradient_y**2)*(self.hessian_xx + self.hessian_yy)).mean()
+
+		#Quartic moments
+		K0 = (self.kappa**4).mean()
+		K1 = ((self.kappa**3) * (self.hessian_xx + self.hessian_yy)).mean()
+		K2 = ((self.kappa) * (self.gradient_x**2 + self.gradient_y**2) * (self.hessian_xx + self.hessian_yy)).mean()
+		K3 = ((self.gradient_x**2 + self.gradient_y**2)**2).mean()
+
+		#Compute connected moments (only quartic affected)
+		if connected:
+			K0 -= sigma0**4
+			K1 += 3 * sigma0**2 * sigma1**2
+			K2 += sigma1**4
+			K3 -= 2 * sigma1**4
+
+		
+		#Normalize moments to make them dimensionless
+		if dimensionless:
+			S0 /= sigma0**3
+			S1 /= (sigma0 * sigma1**2)
+			S2 *= (sigma0 / sigma1**4)
+
+			K0 /= sigma0**4
+			K1 /= (sigma0**2 * sigma1**2)
+			K2 /= sigma1**4
+			K3 /= sigma1**4
+
+			sigma0 /= sigma0
+			sigma1 /= sigma1
+
+		#Return the array
+		return np.array([sigma0,sigma1,S0,S1,S2,K0,K1,K2,K3])
