3bdf82257a24fb2da13e09de85e44b461261cea8
apetri <apetri@phys.columbia.edu>
2014-07-14 20:07:34 -0600

    Completed power spectrum indexer

--- lenstools/index.py
+++ lenstools/index.py
@@ -21,7 +21,8 @@ class Descriptor(object):
 	__metaclass__ = ABCMeta
 
 	def __init__(self):
-		self.label = None
+		self._first = None
+		self._last = None
 		self._start = 0
 
 	@abstractproperty
@@ -33,6 +34,9 @@ class Descriptor(object):
 		pass
 
 class PowerSpectrum(Descriptor):
+	"""
+	Power spectrum type descriptor class
+	"""
 	
 	def __init__(self,l_edges):
 		
@@ -42,34 +46,50 @@ class PowerSpectrum(Descriptor):
 
 	def __str__(self):
 
-		return "Power Spectrum"
+		return "Power Spectrum descriptor: {0} bins\nEdges: {1}\nMidpoints{2}".format(len(self.l),self.l_edges.__str__(),self.l.__str__())
 
+	@property
 	def first(self):
-		return self._start
 
+		self._first = self._start
+		return self._first
+
+	@property
 	def last(self):
-		return self._start + len(self.l) - 1
+
+		self._last = self._start + len(self.l)
+		return self._last
 
 ###################################
 #####Global indexer################
 ###################################
 
 class Indexer(object):
+	"""
+	This class is useful for indexing array of statistical descriptors that are hstacked together
+
+	:param descriptor_list: list of Descriptor subinstances, such as PowerSpectrum; each of these sub instances must be a subclass of Descriptor with a 'first' and 'last' getter methods implemented
+	:type descriptor_list: list.
+
+	"""
 
 	def __init__(self,descriptor_list):
 
 		#Initialize index with descriptor list
 		self.descriptor_list = descriptor_list
 		self._last = 0
+		self.num_descriptors = len(descriptor_list)
 
 		#Create an index of all the descriptor bounds
 		for descriptor in descriptor_list:
 
-			increment = descriptor.last() + 1
+			increment = descriptor.last
 			descriptor._start = self._last
 			self._last += increment
 
 	def __getitem__(self,n):
+		"""Allows to access the n-th descriptor by indexing"""
+
 		return self.descriptor_list[n]
 			
 
