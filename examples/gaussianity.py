f521d0d9a84204272b019d3426c2701ac61862f2
apetri <apetri@phys.columbia.edu>
2014-07-16 20:27:50 -0600

    Parallelized gaussianity.py with MPI Pool

--- examples/gaussianity.py
+++ examples/gaussianity.py
@@ -2,11 +2,15 @@
 Tests the gaussianity of simulated noise maps by measuring their cubic and quartic moments
 
 """
+import sys
 
 from lenstools import Ensemble,GaussianNoiseGenerator
 import numpy as np
 
 import logging
+
+from emcee.utils import MPIPool
+
 
 def generate_and_measure(args):
 
@@ -21,15 +25,28 @@ def generate_and_measure(args):
 	#Measure its moments
 	return conv_map.moments(connected=True,dimensionless=True)
 
-logging.basicConfig(level=logging.DEBUG)
+logging.basicConfig(level=logging.INFO)
 
-map_ids = range(4)
+try: 
+	pool = MPIPool()
+except ValueError:
+	pool = None
+
+if (pool is not None) and not(pool.is_master()):
+
+	pool.wait()
+	sys.exit(0)
+
+map_ids = range(int(sys.argv[1]))
 
 gen = GaussianNoiseGenerator(shape=(2048,2048),side_angle=3.41,label="convergence") 
 power_func = np.loadtxt("ee4e-7.txt",unpack=True)
 
 ens = Ensemble.fromfilelist(map_ids)
 
-ens.load(callback_loader=generate_and_measure,pool=None,generator=gen,power_func=power_func)
+ens.load(callback_loader=generate_and_measure,pool=pool,generator=gen,power_func=power_func)
+if pool is not None:
+	pool.close()
 
 np.savetxt("moments.txt",np.array([ens.mean(),np.sqrt(ens.covariance().diagonal())]))
+logging.info("Done!")
