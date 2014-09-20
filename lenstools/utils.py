583851838e3af1b9f79a82277d27d20396c751aa
apetri <apetri@phys.columbia.edu>
2014-09-20 14:47:15 -0400

    added some RMA utilities to MPIPool

--- lenstools/utils.py
+++ lenstools/utils.py
@@ -1,6 +1,13 @@
 from __future__ import division
 import numpy as np
 
+from emcee.utils import MPIPool
+
+try:
+	from mpi4py import MPI
+except:
+	pass
+
 #####################################################################################
 #######Supplying to the lack of rfftfreq implementation in numpy<1.8#################
 #####################################################################################
@@ -34,3 +41,62 @@ class _interpolate_wrapper(object):
 			print(" exception:")
 			traceback.print_exc()
 			raise
+
+
+#################################################################################################
+###################MPIWhirlPool: should handle one sided communications too######################
+#################################################################################################
+
+class MPIWhirlPool(MPIPool):
+
+	"""
+	MPI class handler, inherits from MPI pool and adds one sided communications utilities (using RMA windows)
+
+	"""
+
+	def openWindow(self,memory):
+
+		"""
+		Create a RMA window that looks from the master process onto all the other workers
+
+		:param memory: memory buffer on which to open the window
+		:type memory: numpy nd array
+
+		"""
+
+		#Stats of the memory to open a window onto
+		assert isinstance(memory,np.ndarray)
+		self.mpi_data_type = MPI.FLOAT
+		self.read_buffer = np.zeros(memory.shape,dtype=memory.dtype)
+
+		#Create the window
+		self.win = MPI.Win.Create(memory=memory,comm=self.comm)
+		self.win.Fence()
+
+	
+	def get(self,process):
+
+		"""
+		Read data from an RMA window open on a particular process
+
+		"""
+
+		if self.is_master():
+			self.win.Get([self.read_buffer,self.mpi_data_type],process)
+
+		self.win.Fence()
+
+		if self.is_master():
+			return self.read_buffer
+		else:
+			return None
+
+
+	def closeWindow(self):
+
+		"""
+		Closes a previously opened RMA window
+
+		"""
+
+		self.win.Free()
