82106e8932d2c159d9b968b868e86e4d59e37131
apetri <apetri@phys.columbia.edu>
2014-09-23 18:01:49 -0400

    update 3d density

--- visualization/3d_density.py
+++ visualization/3d_density.py
@@ -1,9 +1,43 @@
-import numpy as np
-from scipy.ndimage import filters
 
+from lenstools.simulations import Gadget2Snapshot
+from lenstools.utils import MPIWhirlPool
+from scipy.ndimage import filters
 from mayavi import mlab
 
-n = np.load("density_parallel.npy")
-ns = filters.gaussian_filter(n,2)
+#Initialize MPIWhirlPool
+try:
+	pool = MPIWhirlPool()
+except:
+	pool = None
 
-scene = mlab.pipeline.volume(mlab.pipeline.scalar_field(ns))
+
+if pool is None:
+
+	print("Processing ICs in series...")
+
+	#If we run on one core only the snapshots need to be processed in series
+	snap = Gadget2Snapshot.open("Test/Data/gadget/ic1.0")
+	n1,r = snap.numberDensity(resolution=64,left_corner=np.array([0.0,0.0,0.0])*snap.Mpc_over_h)
+	snap.close()
+
+	snap = Gadget2Snapshot.open("Test/Data/gadget/ic1.1")
+	n2,r = snap.numberDensity(resolution=64,left_corner=np.array([0.0,0.0,0.0])*snap.Mpc_over_h)
+	snap.close()
+
+	n = n1+n2
+	ns = filters.gaussian_filter(n,2)
+	scene = mlab.pipeline.volume(mlab.pipeline.scalar_field(ns))
+
+else:
+
+	if pool.is_master():
+		print("Processing ICs in parallel on {0} cores".format(pool.size+1))
+
+	#If we run on 2 cores, we can process the snapshots in parallel
+	snap = Gadget2Snapshot.open("Test/Data/gadget/ic1",pool=pool)
+	n,r = snap.numberDensity(resolution=64,left_corner=np.array([0.0,0.0,0.0])*snap.Mpc_over_h)
+	snap.close()
+
+	if pool.is_master():
+		ns = filters.gaussian_filter(n,2)
+		scene = mlab.pipeline.volume(mlab.pipeline.scalar_field(ns))
