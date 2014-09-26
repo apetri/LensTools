dea3b005a25cd8ed91203d8505b31c3c8ddd25bf
apetri <apetri@phys.columbia.edu>
2014-09-26 15:32:48 -0400

    layed down skeleton classes for ray tracing

--- lenstools/simulations/__init__.py
+++ lenstools/simulations/__init__.py
@@ -2,7 +2,7 @@
 
 .. module:: simulations 
 	:platform: Unix
-	:synopsis: This module handles the book keeping of simulation sets map names, cosmological parameters, etc...
+	:synopsis: This module handles the book keeping of simulation sets map names, cosmological parameters, etc... It also provides an API to interact with Gadget2 snapshots and a prototype ray tracing engine
 
 
 .. moduleauthor:: Andrea Petri <apetri@phys.columbia.edu>
@@ -14,3 +14,4 @@ from .design import Design
 from .igs1 import IGS1
 from .cfhtemu1 import CFHTemu1,CFHTcov
 from .gadget2 import Gadget2Snapshot
+from .raytracing import LensPlane
