5b3309c97b70d414227968b8f30bdc09fca0c27a
apetri <apetri@phys.columbia.edu>
2015-02-20 14:39:42 -0500

    decide which parameters to put in cosmo_id

--- lenstools/pipeline/simulation.py
+++ lenstools/pipeline/simulation.py
@@ -4,6 +4,16 @@ from astropy.cosmology import FLRW,WMAP9
 
 from .environment import EnvironmentSettings
 
+name2attr = dict()
+name2attr["Om"] = "Om0"
+name2attr["Ol"] = "Ode0"
+name2attr["w"] = "w0"
+name2attr["wa"] = "wa"
+name2attr["h"] = "h"
+name2attr["Ob"] = "Ob0"
+name2attr["si"] = "sigma8"
+name2attr["ns"] = "ns"
+
 
 ################################################
 ##############Simulation class##################
@@ -16,7 +26,7 @@ class Simulation(object):
 
 	"""
 
-	def __init__(self,cosmology=WMAP9,environment=None):
+	def __init__(self,cosmology=WMAP9,environment=None,parameters=["Om","Ol","w","ns","si"]):
 
 		"""
 		Set the base for the simulation
@@ -26,6 +36,9 @@ class Simulation(object):
 
 		:param environment: environment settings of the current machine
 		:type environment: EnvironmentSettings
+
+		:param parameters: cosmological parameters to keep track of
+		:type parameters: list.
 
 		"""
 
@@ -40,21 +53,7 @@ class Simulation(object):
 
 		self.cosmology = cosmology
 		self.environment = environment
+		self.parameters = parameters
 
 		#Build the cosmo_id
-		self.cosmo_id = "h{0:.3f}_Om{1:.3f}_Ol{2:.3f}".format(self.cosmology.h,self.cosmology.Om0,self.cosmology.Ode0)
-		
-		if (hasattr(self.cosmology,"Ob0")) and (getattr(self.cosmology,"Ob0") is not None):
-			self.cosmo_id += "_Ob{0:.3f}".format(getattr(self.cosmology,"Ob0"))
-
-		if (hasattr(self.cosmology,"w0")) and (getattr(self.cosmology,"w0") is not None):
-			self.cosmo_id += "_w{0:.3f}".format(getattr(self.cosmology,"w0"))
-
-		if (hasattr(self.cosmology,"wa")) and (getattr(self.cosmology,"wa") is not None):
-			self.cosmo_id += "_wa{0:.3f}".format(getattr(self.cosmology,"wa"))
-
-		if (hasattr(self.cosmology,"ns")) and (getattr(self.cosmology,"ns") is not None):
-			self.cosmo_id += "_ns{0:.3f}".format(getattr(self.cosmology,"ns"))
-
-		if (hasattr(self.cosmology,"sigma8")) and (getattr(self.cosmology,"sigma8") is not None):
-			self.cosmo_id += "_si{0:.3f}".format(getattr(self.cosmology,"sigma8"))
+		self.cosmo_id = "_".join([ "{0}{1:.3f}".format(p,getattr(self.cosmology,name2attr[p])) for p in parameters if (hasattr(self.cosmology,name2attr[p]) and getattr(self.cosmology,name2attr[p]) is not None)])
