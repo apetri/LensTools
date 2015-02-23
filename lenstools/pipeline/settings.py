6f54c34ed5fe0f73ac1a39c0346b6753b4023721
apetri <apetri@phys.columbia.edu>
2015-02-23 17:53:38 -0500

    added NGenIC settings class

---
+++ lenstools/pipeline/settings.py
@@ -0,0 +1,12 @@
+
+
+#################################################
+###########NGenICSettings class##################
+#################################################
+
+class NGenICSettings(object):
+
+	"""
+	Class handler of NGenIC settings
+	
+	"""
