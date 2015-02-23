ac17037254d930eae42534b7d145407ba6982ce9
apetri <apetri@phys.columbia.edu>
2015-02-23 17:48:51 -0500

    detect existent simulations

--- Test/test_pipeline.py
+++ Test/test_pipeline.py
@@ -10,25 +10,28 @@ except ImportError:
 	from lenstools.pipeline import EnvironmentSettings,SimulationModel,SimulationCollection,SimulationIC
 	from lenstools.simulations import Nicaea
 
-seeds = [0,11,222]
 home = "../SimTest/Home"
 storage = "../SimTest/Storage"
 
+cosmologies = [Nicaea(),Nicaea(wa=0.5)]
 box_sizes = [15.0,240.0]
 part = [32,512]
+seeds = [0,11,222]
 
 #Check environment settings
 env = EnvironmentSettings(home=home,storage=storage)
 
 def test_directoryTree():
 
-	cosmoFiducial = Nicaea()
+	#Create two simulation models
+	for cosmo in cosmologies:
+	
+		simulation_model = SimulationModel(cosmology=cosmo,environment=env,parameters=["Om","Ol","w","wa"])
 
-	#Create new simulation model
-	simulation_model = SimulationModel(cosmology=cosmoFiducial,environment=env,parameters=["Om","Ol","w","wa"])
-
-	#Create two different collections, three initial conditions per collection
-	for i,box_size in enumerate(box_sizes):
-		collection = simulation_model.newCollection(box_size=box_size*simulation_model.Mpc_over_h,nside=part[i])
-		for seed in seeds:
-			ic = collection.newInitialCondition(seed)
+		#Create two different collections, three initial conditions per collection
+		for i,box_size in enumerate(box_sizes):
+			
+			collection = simulation_model.newCollection(box_size=box_size*simulation_model.Mpc_over_h,nside=part[i])
+			
+			for seed in seeds:
+				ic = collection.newInitialCondition(seed)
--- lenstools/pipeline/simulation.py
+++ lenstools/pipeline/simulation.py
@@ -1,11 +1,12 @@
 import os,glob
+import re
 
 import astropy.units as u
 from astropy.cosmology import FLRW,WMAP9
 
 
 from .environment import EnvironmentSettings
-from ..simulations import Gadget2Settings 
+from ..simulations import Gadget2Settings,Nicaea 
 
 name2attr = dict()
 name2attr["Om"] = "Om0"
@@ -28,6 +29,65 @@ class SimulationModel(object):
 	Class handler of a weak lensing simulation model, defined by a set of cosmological parameters
 
 	"""
+
+	@classmethod
+	def available(cls,environment):
+
+		"""
+		Lists all currently available models in the home and storage directories
+
+		:param environment: environment settings of the current machine
+		:type environment: EnvironmentSettings
+
+		:returns: list.
+		:rtype: SimulationModel
+
+		"""
+
+		models = list()
+
+		#Useful regular expression
+		parmatch = re.compile(r"([a-zA-Z]+)([0-9.-]+)")
+
+		if not(os.path.isdir(environment.home)) or not(os.path.isdir(environment.storage)):
+			return models
+
+		#Check all available models in the home directory
+		dirnames = [ os.path.basename(n) for n in glob.glob(os.path.join(environment.home,"*")) ]
+
+		#Decide which of the directories actually correspond to cosmological models
+		for dirname in dirnames:
+			
+			parameters_dict = dict()
+			parameters_list = list()
+			parameters = dirname.split("_")
+			
+			for parameter in parameters:
+				
+				try:
+					par,val = parmatch.match(parameter).groups()
+				except TypeError:
+					pass
+				
+				parameters_list.append(par)
+				parameters_dict[name2attr[par]] = float(val)
+
+			try:
+				cosmoModel = Nicaea(**parameters_dict)
+				models.append(cls(cosmology=cosmoModel,environment=environment,parameters=parameters_list))
+			except TypeError:
+				pass
+
+		#Return the list with the available models
+		return models
+
+	def __repr__(self):
+
+		representation_parameters = []
+		for p in self.parameters:
+			representation_parameters.append("{0}={1:.3f}".format(p,getattr(self.cosmology,name2attr[p])))
+
+		return "<"+ " , ".join(representation_parameters) + ">"
 
 	def __init__(self,cosmology=WMAP9,environment=None,parameters=["Om","Ol","w","ns","si"]):
 
@@ -93,6 +153,31 @@ class SimulationModel(object):
 		return newSimulation
 
 
+	@property
+	def collections(self):
+
+		"""
+		Lists all the available collections for a model
+
+		:returns: list.
+		:rtype: SimulationCollection
+
+		"""
+
+		collection_names = [ os.path.basename(d) for d in glob.glob(os.path.join(self.home_subdir,"*")) ]
+		collection_list = list()
+
+		for name in collection_names:
+			
+			try:
+				nside,box = name.split("b")
+				collection_list.append(SimulationCollection(self.cosmology,self.environment,self.parameters,box_size=float(box)*self.Mpc_over_h,nside=int(nside)))
+			except ValueError:
+				pass
+
+		return collection_list
+
+
 ##########################################################
 ##############SimulationCollection class##################
 ##########################################################
@@ -119,6 +204,11 @@ class SimulationCollection(SimulationModel):
 		self.storage_subdir = os.path.join(self.environment.storage,self.cosmo_id,self.geometry_id)
 
 
+	def __repr__(self):
+
+		return super(SimulationCollection,self).__repr__() + " ; box={0},nside={1}".format(self.box_size,self.nside)
+
+
 	def newCollection(self):
 		raise TypeError("This method should be called on SimulationModel instances!")
 
@@ -142,6 +232,28 @@ class SimulationCollection(SimulationModel):
 		return newIC
 
 
+	@property
+	def ics(self):
+
+		"""
+		List the available initial conditions (or independent simulations) for the current collection
+
+		:returns: list.
+		:rtype: SimulationIC
+
+		"""
+
+		ic_list = list()
+		ic_numbers = [ os.path.basename(d).strip("ic") for d in glob.glob(os.path.join(self.storage_subdir,"ic*")) ]
+
+		for ic in ic_numbers:
+
+			seed = int(os.path.basename(glob.glob(os.path.join(self.storage_subdir,"ic"+ic,"seed*"))[0]).strip("seed"))
+			ic_list.append(SimulationIC(self.cosmology,self.environment,self.parameters,self.box_size,self.nside,int(ic),seed))
+
+		return ic_list
+
+
 
 ##########################################################
 ##############SimulationIC class##########################
@@ -159,9 +271,14 @@ class SimulationIC(SimulationCollection):
 		super(SimulationIC,self).__init__(cosmology,environment,parameters,box_size,nside)
 
 		#Save random seed information as attribute
+		self.ic_index = ic_index
 		self.seed = seed
 
 		#Save storage sub-directory name
 		self.storage_subdir = os.path.join(self.storage_subdir,"ic{0}".format(ic_index))
+
+	def __repr__(self):
+
+		return super(SimulationIC,self).__repr__() + " ; ic={0},seed={1}".format(self.ic_index,self.seed)
 
 
