90b08330895bd24337dd28145d7a2587eae26379
apetri <apetri@phys.columbia.edu>
2015-03-16 12:24:36 -0400

    debugged remote (not fully functional yet)

--- lenstools/pipeline/remote.py
+++ lenstools/pipeline/remote.py
@@ -1,6 +1,12 @@
 from abc import ABCMeta,abstractproperty,abstractmethod
 import os,glob
 
+try:
+	from paramiko import SSHClient
+	SSHClient = SSHClient
+except ImportError:
+	SSHClient = None
+
 ###################################################
 ###########SystemHandler class#####################
 ###################################################
@@ -34,9 +40,9 @@ class SystemHandler(object):
 		pass
 
 
-#############################################
-#########Local filesystem ###################
-#############################################
+############################################
+#########Local filesystem###################
+############################################
 
 class LocalSystem(SystemHandler):
 
@@ -50,6 +56,7 @@ class LocalSystem(SystemHandler):
 	#############################################
 
 	def __init__(self,readonly=False):
+		
 		self.name = "localhost"
 		self.readonly = readonly
 
@@ -72,3 +79,68 @@ class LocalSystem(SystemHandler):
 			raise IOError("Simulation batch is read only!")
 
 		return open(f,mode)
+
+
+##########################################################
+#########Remote Unix filesystem via SSH###################
+##########################################################
+
+class UnixSSH(SystemHandler):
+
+	"""
+	Unix SSH client
+
+	"""
+
+	#############################################
+	######Abstract method definitions############
+	#############################################
+
+	def __init__(self,client,readonly=True):
+
+		if SSHClient is None:
+			raise ImportError("paramiko needs to be installed to use remote SSH functionality!")
+
+		if not(readonly):
+			raise NotImplementedError
+
+		assert isinstance(client,SSHClient)
+		self.client = client
+		self.readonly = readonly
+
+		#Assign IP address and port as name
+		host_ip,port = self.client.get_transport().getpeername()
+		print("[+] System handler will read/write from {0}:{1}".format(host_ip,port))
+		self.name = "{0}:{1}".format(host_ip,port)
+
+		#Open SFTP session
+		self.sftp = self.client.open_sftp()
+
+	def mkdir(self,d):
+		raise NotImplementedError
+
+	def exists(self,d):
+
+		stdin,stdout,stderr = self.client.exec_command("ls -d {0}".format(d))
+		result = stdout.read().strip("\n")
+
+		if result=="":
+			return False
+		else:
+			return True
+
+	def glob(self,n):
+
+		stdin,stdout,stderr = self.client.exec_command("ls -d {0}".format(n))
+		return [ d.rstrip("\n").rstrip(":") for d in stdout.readlines("\n") ]
+
+	def open(self,f,mode):
+
+		if (self.readonly) and ("w" in mode or "a" in mode):
+			raise IOError("Simulation batch is read only!") 
+
+		if "w" in mode or "a" in mode:
+			raise NotImplementedError
+
+		return self.sftp.file(f,mode)
+
