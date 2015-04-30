1c5c3ad75c765d6bdd88219f6097bb8dda0ae3c3
apetri <apetri@phys.columbia.edu>
2015-04-30 10:12:03 -0400

    install all dependencies on travis build

--- setup.py
+++ setup.py
@@ -232,7 +232,7 @@ for ext_module in external_sources.keys():
 package_data = dict()
 package_data[name] = [ os.path.join("data",filename) for filename in os.listdir(os.path.join(name,"data")) if os.path.isfile(os.path.join(name,"data",filename)) ]
 package_data[name] += [ os.path.join(external_dir,filename) for filename in os.listdir(os.path.join(name,external_dir)) if filename.endswith(".h") ] 
-package_data["licenses"] = [ "LICENSE.rst" ]
+package_data[""] = [ os.path.join("licenses","LICENSE.rst") ]
 
 #################################################################################################
 #############################Additional includes#################################################
