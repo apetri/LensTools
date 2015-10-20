fixed factor of 2, x<->y bugs

lenstools/statistics/constraints.py
@@ -394,7 +394,7 @@ class Analysis(Ensemble):
 
 		#Compute the directions and sizes of the ellipse axes
 		w,v = np.linalg.eigh(covariance)
-		width,height = np.sqrt(w * stats.chi2(2).ppf(p_value))
+		width,height = 2*np.sqrt(w * stats.chi2(2).ppf(p_value))
 
 		try:
 			angle = 180.*np.arctan(v[1,0] / v[0,0]) / np.pi

lenstools/statistics/contours.py
@@ -661,8 +661,8 @@ class ContourPlot(object):
 
 		#Compute the hessian
 		hessian = np.zeros((2,2))
-		hessian[0,0] = (likelihood[imax,jmax+2] + likelihood[imax,jmax-2] - 2*likelihood[imax,jmax]) / 4.
-		hessian[1,1] = (likelihood[imax+2,jmax] + likelihood[imax-2,jmax] - 2*likelihood[imax,jmax]) / 4.
+		hessian[0,0] = (likelihood[imax+2,jmax] + likelihood[imax-2,jmax] - 2*likelihood[imax,jmax]) / 4.
+		hessian[1,1] = (likelihood[imax,jmax+2] + likelihood[imax,jmax-2] - 2*likelihood[imax,jmax]) / 4.
 		hessian[1,0] = (likelihood[imax+1,jmax+1] + likelihood[imax-1,jmax-1] - likelihood[imax+1,jmax-1] - likelihood[imax-1,jmax+1]) / 4.
 
 		#Scale the hessian to parameter units