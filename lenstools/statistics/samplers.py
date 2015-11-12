99decb1c4bf6a9689b8f5e9a65e0e3c0fbf7bb12
apetri <apetri@phys.columbia.edu>
2015-11-12 16:11:24 -0500

    allow slicing of MCMC sampling space

--- lenstools/statistics/samplers.py
+++ lenstools/statistics/samplers.py
@@ -29,15 +29,27 @@ def multiquadric(x,e):
 ##Log of the gaussian likelihood##
 ##################################
 
-def lnprobgauss(p,emulator,data,icov):
-	diff = data - emulator.predict(p,raw=True)
+def lnprobgauss(p,emulator,data,icov,pslice_values,sample_indices):
+
+	if pslice_values is not None:
+		
+		pc = np.empty(len(pslice_values)+len(sample_indices))
+		for i in pslice_values:
+			pc[i] = pslice_values[i]
+		for n,i in enumerate(sample_indices):
+			pc[i] = p[n]
+
+	else:
+		pc =p
+
+	diff = data - emulator.predict(pc,raw=True)
 	return -0.5*diff.dot(icov).dot(diff)
 
 ###############################
 ######emcee sampler############
 ###############################
 
-def emcee_sampler(emulator,observed_feature,features_covariance,nwalkers=16,nburn=100,nchain=1000,pool=None):
+def emcee_sampler(emulator,observed_feature,features_covariance,pslice=None,nwalkers=16,nburn=100,nchain=1000,pool=None):
 
 	"""
 	Parameter posterior sampling based on the MCMC algorithm implemented by the emcee package
@@ -50,6 +62,9 @@ def emcee_sampler(emulator,observed_feature,features_covariance,nwalkers=16,nbur
 
 	:param features_covariance: covariance matrix of the features
 	:type features_covariance: array
+
+	:param pslice: specify slices of the parameter space in which some parameters are keps as constants
+	:type pslice: dict.
 
 	:param nwalkers: number of chains
 	:type nwalkers: int.
@@ -70,9 +85,21 @@ def emcee_sampler(emulator,observed_feature,features_covariance,nwalkers=16,nbur
 
 	#Train the emulator with fast interpolation
 	emulator.train(method=multiquadric)
+	parameters = emulator["parameters"]
+	parameters_to_sample = emulator.parameter_names
 
 	#Parameter space to sample
-	parameters = emulator["parameters"].values
+	if pslice is None: 
+		parameters = parameters.values
+		pslice_values = None
+		sample_indices = None
+	else:
+		pslice_values = dict((emulator.parameter_names.index(p),pslice[p]) for p in pslice)
+		sample_indices = [ emulator.parameter_names.index(p) for p in emulator.parameter_names if p not in pslice ]
+		assert len(pslice_values)+len(sample_indices)==len(emulator.parameter_names)
+		parameters = parameters.values[:,sample_indices]
+
+	#Extremes of the sampling space	
 	pmin,pmax = parameters.min(0),parameters.max(0)
 
 	#Feature name
@@ -86,7 +113,7 @@ def emcee_sampler(emulator,observed_feature,features_covariance,nwalkers=16,nbur
 	p0 = pmin + np.random.uniform(size=(nwalkers,ndim))*(pmax-pmin)
 
 	#Initialize the sampler
-	sampler = emcee.EnsembleSampler(nwalkers,ndim,lnprobgauss,args=[emulator,observed_feature,icov],pool=pool)
+	sampler = emcee.EnsembleSampler(nwalkers,ndim,lnprobgauss,args=[emulator,observed_feature,icov,pslice_values,sample_indices],pool=pool)
 
 	#Burn-in
 	logdriver.info("Running emcee burn-in: feature name={0}, feature dimension={1}, parameter dimension={2}, steps={3}".format(feature_name,len(observed_feature),ndim,nburn))
@@ -98,4 +125,7 @@ def emcee_sampler(emulator,observed_feature,features_covariance,nwalkers=16,nbur
 	sampler.run_mcmc(pos,nchain,rstate0=state)
 
 	#Return sampled parameters
-	return Ensemble(sampler.flatchain,columns=emulator.parameter_names)
+	if pslice is None:
+		return Ensemble(sampler.flatchain,columns=emulator.parameter_names)
+	else:
+		return Ensemble(sampler.flatchain,columns=filter(lambda p:p not in pslice,emulator.parameter_names))
