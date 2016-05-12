fbc5e48597e801ba12ee3991683dba3281f9f588
apetri <apetri@phys.columbia.edu>
2016-05-12 12:59:26 -0400

    fixed Gadget2Settings not found

--- lenstools/simulations/gadget2.py
+++ lenstools/simulations/gadget2.py
@@ -1,12 +1,198 @@
 from __future__ import division
 import os
+import StringIO
 
 from .nbody import NbodySnapshot
 from .. import extern as ext
+from .settings import LTSettings
 
 import numpy as np
-from astropy.units import Mbyte,kpc,Mpc,Msun,cm,km,g,s,hour,day,quantity,def_unit
+import astropy.units as u
 from astropy.cosmology import w0waCDM,LambdaCDM
+
+############################################################
+################Gadget2Settings class#######################
+############################################################
+
+class Gadget2Settings(LTSettings):
+
+	"""
+	Class handler of the tunable settings in a Gadget2 run
+
+	"""
+
+	file_names = ["InitCondFile","OutputDir","EnergyFile","InfoFile","TimingsFile","CpuFile","RestartFile","SnapshotFileBase","OutputListFilename"]
+	cpu_timings = ["TimeLimitCPU","ResubmitOn","ResubmitCommand"]
+	code_options = ["ICFormat","SnapFormat","ComovingIntegrationOn","TypeOfTimestepCriterion","OutputListOn","PeriodicBoundariesOn"]
+	characteristics_of_run = ["TimeMax"]
+	output_frequency = ["TimeBetSnapshot","TimeOfFirstSnapshot","CpuTimeBetRestartFile","TimeBetStatistics","NumFilesPerSnapshot","NumFilesWrittenInParallel"]
+	accuracy_time_integration = ["ErrTolIntAccuracy","MaxRMSDisplacementFac","CourantFac","MaxSizeTimestep","MinSizeTimestep"]
+	tree_algorithm = ["ErrTolTheta","TypeOfOpeningCriterion","ErrTolForceAcc","TreeDomainUpdateFrequency"]
+	sph = ["DesNumNgb","MaxNumNgbDeviation","ArtBulkViscConst","InitGasTemp","MinGasTemp"]
+	memory_allocation = ["PartAllocFactor","TreeAllocFactor","BufferSize"]
+	system_of_units = ["UnitLength_in_cm","UnitMass_in_g","UnitVelocity_in_cm_per_s","GravityConstantInternal"]
+	softening = ["MinGasHsmlFractional","SofteningGas","SofteningHalo","SofteningDisk","SofteningBulge","SofteningStars","SofteningBndry","SofteningGasMaxPhys","SofteningHaloMaxPhys","SofteningDiskMaxPhys","SofteningBulgeMaxPhys","SofteningStarsMaxPhys","SofteningBndryMaxPhys"]
+
+	def __init__(self,**kwargs):
+
+		#Default outputs
+		self.OutputScaleFactor = np.array([0.2463687286034,0.25331915596808,0.2603792960781,0.26755062217624,0.27483471475984,0.282233268286,0.28974809827109,0.29738114881152,0.30513450055509,0.31301037915502,0.32101116424033,0.32913939894726,0.3373978000372,0.34578926866836,0.35431690185511,0.36298400467612,0.37179410329025,0.38075095882651,0.38985858222059,0.39912125007794,0.40854352165158,0.4181302570317,0.42788663665433,0.43781818224776,0.4479307793476,0.45823070152614,0.46872463649679,0.47941971427274,0.49032353757851,0.50144421473605,0.51279039527177,0.52437130852031,0.53619680553253,0.54827740463258,0.56062434101017,0.57324962078195,0.58616608000982,0.59938744922579,0.61292842408364,0.62680474283878,0.64103327145057,0.65563209720868,0.67062063190864,0.68601972574487,0.70185179325524,0.71814095284413,0.73491318163551,0.75219648767026,0.77002110176951,0.78841969174729,0.80742760208188,0.82708312265889,0.84742779079644,0.86850673147378,0.89036904153293,0.91306822464037,0.9366626850185,0.96121628943351,0.98679900871668,1.0])
+
+		#File names
+		self.InitCondFile = "gadget_ic"
+		self.OutputDir = "snapshots"
+		self.EnergyFile = "energy.txt"
+		self.InfoFile = "info.txt"
+		self.TimingsFile = "timings.txt"
+		self.CpuFile = "cpu.txt"
+		self.RestartFile = "restart"
+		self.SnapshotFileBase = "snapshot"
+		self.OutputListFilename = "outputs.txt"
+
+		#CPU Timings
+		self.TimeLimitCPU = 1.0*u.day
+		self.ResubmitOn = 0
+		self.ResubmitCommand = "my-scriptfile"
+
+		#Code options
+		self.ICFormat  = 1
+		self.SnapFormat = 1
+		self.ComovingIntegrationOn = 1
+		self.TypeOfTimestepCriterion = 0
+		self.OutputListOn = 1
+		self.PeriodicBoundariesOn = 1
+
+		#Caracteristics of run  
+		self.TimeMax = 1.0
+
+		#Output frequency
+		self.TimeBetSnapshot = 0.5
+		self.TimeOfFirstSnapshot = 0
+		self.CpuTimeBetRestartFile = 12.5*u.hour 
+		self.TimeBetStatistics = 0.05
+		self.NumFilesPerSnapshot = 16
+		self.NumFilesWrittenInParallel = 8
+
+		#Accuracy of time integration
+		self.ErrTolIntAccuracy = 0.025 
+		self.MaxRMSDisplacementFac = 0.2
+		self.CourantFac = 0.15     
+		self.MaxSizeTimestep = 0.02
+		self.MinSizeTimestep = 0.0
+
+
+		#Tree algorithm, force accuracy, domain update frequency
+		self.ErrTolTheta = 0.45
+		self.TypeOfOpeningCriterion = 1
+		self.ErrTolForceAcc = 0.005
+		self.TreeDomainUpdateFrequency = 0.025
+
+		
+		#Further parameters of SPH
+		self.DesNumNgb = 33
+		self.MaxNumNgbDeviation = 2
+		self.ArtBulkViscConst = 0.8
+		self.InitGasTemp = 1000.0    
+		self.MinGasTemp = 50.0    
+
+		#Memory allocation
+		self.PartAllocFactor = 1.3    
+		self.TreeAllocFactor = 0.7
+		self.BufferSize = 20*u.Mbyte 
+
+
+		#System of units
+		self.UnitLength_in_cm = 3.085678e21       # ;  1.0 kpc 
+		self.UnitMass_in_g = 1.989e43    #;  1.0e10 solar masses 
+		self.UnitVelocity_in_cm_per_s = 1.0e5  # ;  1 km/sec 
+		self.GravityConstantInternal = 0
+
+
+		#Softening lengths
+		self.MinGasHsmlFractional = 0.25
+		self.SofteningGas = 0
+		self.SofteningHalo = 9.000000
+		self.SofteningDisk = 0
+		self.SofteningBulge = 0
+		self.SofteningStars = 0
+		self.SofteningBndry = 0
+
+		self.SofteningGasMaxPhys = 0
+		self.SofteningHaloMaxPhys = 9.000000
+		self.SofteningDiskMaxPhys = 0
+		self.SofteningBulgeMaxPhys = 0
+		self.SofteningStarsMaxPhys = 0
+		self.SofteningBndryMaxPhys = 0
+
+		#Allow for kwargs override
+		for key in kwargs.keys():
+			setattr(self,key,kwargs[key])
+
+	def sections(self):
+
+		return [ "file_names","cpu_timings","code_options","characteristics_of_run","output_frequency","accuracy_time_integration","tree_algorithm","sph","memory_allocation","system_of_units","softening" ]
+
+	def showSection(self,section):
+
+		if section not in self.sections():
+			raise ValueError("Parameter file does not admit a section named {0}".format(section))
+
+		for option in getattr(self,section):
+			print("{0} = {1}".format(option,getattr(self,option)))
+
+	def show(self):
+
+		for section in self.sections():
+			print(section+":\n")
+			self.showSection(section)
+			print("\n")	
+
+	def writeSection(self,section):
+
+		"""
+		Writes the corresponding section of the Gadget2 parameter file
+
+		"""
+
+		output = StringIO.StringIO()
+
+		#Write preamble
+		output.write("% {0}\n\n".format(section))
+
+		#Cycle through options
+		for option in getattr(self,section):
+
+			#Read the corresponding value
+			value = getattr(self,option)
+
+			#Convert units as necessary
+			if type(value)==u.quantity.Quantity:
+				
+				if value.unit.physical_type=="time":
+					value = value.to(u.s).value
+				elif value.unit.physical_type=="speed":
+					value = value.to(u.cm/u.s).value
+				elif "byte" in value.unit.to_string():
+					value = value.to(u.Mbyte).value
+
+			#Write the line
+			output.write("{0}		{1}\n".format(option,value))
+
+		#Finish
+		output.write("\n\n")
+		output.seek(0)
+
+		return output.read()
+		
+
+	@classmethod
+	def default(cls):
+
+		"""
+		Generate default settings
+		"""
+
+		return cls()
 
 ############################################################
 ################Gadget2Header class#########################
@@ -91,7 +277,7 @@ class Gadget2Snapshot(NbodySnapshot):
 
 		header["w0"] = -1.0
 		header["wa"] = 0.0
-		header["comoving_distance"] = LambdaCDM(H0=header["h"]*100,Om0=header["Om0"],Ode0=header["Ode0"]).comoving_distance(header["redshift"]).to(kpc).value * header["h"]
+		header["comoving_distance"] = LambdaCDM(H0=header["h"]*100,Om0=header["Om0"],Ode0=header["Ode0"]).comoving_distance(header["redshift"]).to(u.kpc).value * header["h"]
 
 		return header 
 
@@ -152,7 +338,7 @@ class Gadget2Snapshot(NbodySnapshot):
 		try:
 			positions = (ext._gadget2.getPosVel(self.fp,offset,numPart) * self.kpc_over_h).to(self.Mpc_over_h)
 		except AttributeError:
-			positions = ext._gadget2.getPosVel(self.fp,offset,numPart) * kpc
+			positions = ext._gadget2.getPosVel(self.fp,offset,numPart) * u.kpc
 
 		if save:
 			self.positions = positions
@@ -219,7 +405,7 @@ class Gadget2Snapshot(NbodySnapshot):
 
 		#Scale units
 		velocities *= self._velocity_unit
-		velocities *= cm / s
+		velocities *= u.cm / u.s
 
 		if save:
 			self.velocities = velocities
@@ -321,7 +507,7 @@ class Gadget2Snapshot(NbodySnapshot):
 		#Build a bare header based on the available info (need to convert units back to the Gadget ones)
 		_header_bare = self._header.copy()
 		_header_bare["box_size"] = _header_bare["box_size"].to(self.kpc_over_h).value
-		_header_bare["masses"] = _header_bare["masses"].to(g).value * _header_bare["h"] / self._mass_unit
+		_header_bare["masses"] = _header_bare["masses"].to(u.g).value * _header_bare["h"] / self._mass_unit
 		_header_bare["num_particles_file_of_type"] = _header_bare["num_particles_file_of_type"].astype(np.int32)
 		_header_bare["num_particles_total_of_type"] = _header_bare["num_particles_total_of_type"].astype(np.int32)
 		_header_bare["comoving_distance"] = _header_bare["comoving_distance"].to(self.Mpc_over_h).value * 1.0e3
@@ -332,7 +518,7 @@ class Gadget2Snapshot(NbodySnapshot):
 
 		if hasattr(self,"velocities"):
 			assert self.positions.shape==self.velocities.shape
-			_velocities_converted = (self.velocities.to(cm/s).value / self._velocity_unit).astype(np.float32)
+			_velocities_converted = (self.velocities.to(u.cm/u.s).value / self._velocity_unit).astype(np.float32)
 			writeVel = 1
 		else:
 			_velocities_converted = np.zeros((1,3),dtype=np.float32)
@@ -391,7 +577,7 @@ class Gadget2Snapshot(NbodySnapshot):
 	###########################Extra methods####################################################
 	############################################################################################
 
-	def setHeaderInfo(self,Om0=0.26,Ode0=0.74,w0=-1.0,wa=0.0,h=0.72,redshift=100.0,box_size=15.0*Mpc/0.72,flag_cooling=0,flag_sfr=0,flag_feedback=0,flag_stellarage=0,flag_metals=0,flag_entropy_instead_u=0,masses=np.array([0,1.03e10,0,0,0,0])*Msun,num_particles_file_of_type=None,npartTotalHighWord=np.zeros(6,dtype=np.uint32)):
+	def setHeaderInfo(self,Om0=0.26,Ode0=0.74,w0=-1.0,wa=0.0,h=0.72,redshift=100.0,box_size=15.0*u.Mpc/0.72,flag_cooling=0,flag_sfr=0,flag_feedback=0,flag_stellarage=0,flag_metals=0,flag_entropy_instead_u=0,masses=np.array([0,1.03e10,0,0,0,0])*u.Msun,num_particles_file_of_type=None,npartTotalHighWord=np.zeros(6,dtype=np.uint32)):
 
 		"""
 		Sets the header info in the snapshot to write
@@ -414,7 +600,7 @@ class Gadget2Snapshot(NbodySnapshot):
 		self._header["w0"] = w0
 		self._header["wa"] = wa
 		self._header["h"] = h
-		self._header["H0"] = 100.0*h*km/(s*Mpc)
+		self._header["H0"] = 100.0*h*u.km/(u.s*u.Mpc)
 		self._header["redshift"] = redshift
 		self._header["scale_factor"] = 1.0 / (1.0 + redshift)
 		self._header["box_size"] = box_size
@@ -432,8 +618,8 @@ class Gadget2Snapshot(NbodySnapshot):
 		self._header["npartTotalHighWord"] = npartTotalHighWord
 
 		#Define the kpc/h and Mpc/h units for convenience
-		self.kpc_over_h = def_unit("kpc/h",kpc/self._header["h"])
-		self.Mpc_over_h = def_unit("Mpc/h",Mpc/self._header["h"])
+		self.kpc_over_h = u.def_unit("kpc/h",u.kpc/self._header["h"])
+		self.Mpc_over_h = u.def_unit("Mpc/h",u.Mpc/self._header["h"])
 
 		#Compute the comoving distance according to the model
 		cosmo = w0waCDM(H0=100.0*h,Om0=Om0,Ode0=Ode0,w0=w0,wa=wa)
