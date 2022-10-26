98cbed49eb92e2e352f49de70ccccf087f0e3d72
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-10-26 07:10:08 +0000

    Adding tarfile member sanitization to extractall()

--- lenstools/pipeline/simulation.py
+++ lenstools/pipeline/simulation.py
@@ -596,7 +596,29 @@ class SimulationBatch(object):
 	def _unpack(name,path):
 		print("[+] Unpacking {0} into {1}...".format(name,path))
 		with tarfile.open(name,"r:gz") as tar:
-			tar.extractall(path=path)
+
+import os
+
+def is_within_directory(directory, target):
+	
+	abs_directory = os.path.abspath(directory)
+	abs_target = os.path.abspath(target)
+
+	prefix = os.path.commonprefix([abs_directory, abs_target])
+	
+	return prefix == abs_directory
+
+def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+
+	for member in tar.getmembers():
+		member_path = os.path.join(path, member.name)
+		if not is_within_directory(path, member_path):
+			raise Exception("Attempted Path Traversal in Tar File")
+
+	tar.extractall(path, members, numeric_owner=numeric_owner) 
+	
+
+safe_extract(tar, path=path)
 
 	def unpack(self,where,which=None,pool=None):
 
